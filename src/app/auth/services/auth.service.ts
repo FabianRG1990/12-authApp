9f6469733993741950fe2420d1395496c8f57796
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2023-11-07 01:19:37 -0600

    crear el AuthService

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { environments } from 'src/environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  //*readonly es una propiedad que nos permite hacer que el no pueda ser modificado ni usando el mismo servicio
+  private readonly baseUrl: string = environments.baseUrl;
+  private http = inject(HttpClient);
+
+  //private _currentUser = signal<User|null>(null);
+  //private _authStatus = signal<AuhtStatus>();
+
   constructor() { }
+
+
+  login( email: string, password: string ): Observable<boolean> {
+
+    return of(true);
+  }
 }
